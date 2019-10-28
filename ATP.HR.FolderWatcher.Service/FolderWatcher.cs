18d0989aa975bc608c3fb049fd81ed723982b974
Anatolii Dmitryv <anotelio@gmail.com>
2019-10-28 19:42:34 +0200

    Add project files.

---
+++ ATP.HR.FolderWatcher.Service/FolderWatcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Topshelf.FileSystemWatcher;
+
+namespace ATP.HR.FolderWatcher.Service
+{
+    public class FolderWatcher
+    {
+        private readonly Timer timer;
+        
+        private readonly FileSystemEventArgs fileSystemFolderChangedArgs;
+
+        public FolderWatcher(string folderPath, string folderName)
+        {
+            this.fileSystemFolderChangedArgs = new FileSystemEventArgs(WatcherChangeTypes.Changed, folderPath, folderName);
+            FileSystemEventFactory.CreateNormalFileSystemEvent(fileSystemFolderChangedArgs);
+
+            timer = new Timer(1000) { AutoReset = true };
+            timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+        }
+
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+        }
+    }
+}
---
+++ ATP.HR.FolderWatcher.Service/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Topshelf;
+using Topshelf.FileSystemWatcher;
+
+namespace ATP.HR.FolderWatcher.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var exitCode = HostFactory.Run(r =>
+                {
+                    r.Service<FolderWatcher>(s =>
+                    {
+                        s.ConstructUsing(f => new FolderWatcher("D:\\Temp\\hr_data", "hr_data"));
+                        s.WhenStarted(f => f.Start());
+                        s.WhenStopped(f => f.Stop());
+                    });
+
+                    r.RunAsLocalSystem();
+                    r.StartAutomatically();
+
+                    r.SetServiceName("ATP.HR.FolderWatcher.Service");
+                    r.SetDisplayName("ATP.HR.FolderWatcher.Service");
+                    r.SetDescription("This is the service for folder watching zeus files in order to start SQL agent job with SSIS solutions which load these files into hr_reporting database.");
+                });
+
+                int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+                Environment.ExitCode = exitCodeValue;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
